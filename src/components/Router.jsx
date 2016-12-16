8b79df9d660ebbee5da2d39029a48af99a25bcd7
Tyler Miller <tylermillerhome@gmail.com>
2016-12-16 13:59:20 -0800

    cleaned out more confusing files

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Match, Miss } from 'react-router';
+import Main from './Main';
 
 class Router extends Component {
   render() {
     return (
       <div>
-        <Match pattern="/" exactly component={} />
+        <Match pattern="/" exactly component={Main} />
       </div>
     )
   }
